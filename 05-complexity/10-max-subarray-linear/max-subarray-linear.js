71195656e604ccbcce653654770366dd526379bc
Lakhwinderr <lakhwinder.code@gmail.com>
2024-02-24 15:16:45 +0530

    modified:   05-complexity/10-max-subarray-linear/max-subarray-linear-test.js 	modified:   05-complexity/10-max-subarray-linear/max-subarray-linear.js 	new file:   learning-material/time.md

--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -1,3 +1,20 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+    let max = 0;
+    for(let i = 0; i < k; i++){
+        max+=arr[i];
+    }
+    let sum = max;
+    for(let j = k; j < arr.length; j++){
+      sum = sum + arr[j] - arr[j - k];
+      if(sum > max){
+        max = sum;
+      }
+    }
+    return max;
+}
+// const arr1 = [-2, -5, -3, -1, -11, -7, -6, -4];
+// const k1 = 3;
+// console.log(maxSubarraySum(arr1, k1));
+
 
 module.exports = maxSubarraySum;
---
+++ learning-material/time.md
@@ -0,0 +1,5 @@
+```
+console.time("string here");//u started the timer
+console.timeLog("string here")// will output the time elapsed
+console.timeEnd("same string here")  //will console the timeelapsed the console.time and end the timer;
+```
